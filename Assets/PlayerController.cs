f3842953c23a0531ab7cc55cf0598bbc743d0d07
pecarprimoz <pp1508@student.uni-lj.si>
2018-01-04 22:30:29 +0100

    Initial commit, added player controls

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speedRotate;
+    public float speedMove;
+    public float hoverSpeed;
+    public Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb2D.AddForce(transform.up * hoverSpeed);
+            transform.position += transform.up * speedMove * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(transform.forward * speedRotate);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(transform.forward * (-1*speedRotate));
+        }
+        if (hoverSpeed >= 0.1f)
+        {
+            hoverSpeed = 0.1f;
+        }
+    }
+}
